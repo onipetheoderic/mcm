6b8e0332a70baa3d0c09a956f7f5a7cd95ddbae1
theoderic <onipetheoderic@gmail.com>
2019-07-22 09:43:26 +0100

    all

--- app.js
+++ app.js
@@ -133,17 +133,17 @@ app.use((req, res, next) => {
 // });
 
 
-// // // error handler
-// app.use((err, req, res, next) => {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   // res.send('what???', 404);
-//   res.render('errorpage/index', {layout: false, error_code: 404})
-// });
+  // render the error page
+  res.status(err.status || 500);
+  // res.send('what???', 404);
+  res.render('errorpage/index', {layout: false, error_code: 404})
+});
 // EXPRESS MESSAGES MIDDLEWARE copied from https://github.com/expressjs/express-messages
 app.use(flash());
 app.use(function (req, res, next) {
